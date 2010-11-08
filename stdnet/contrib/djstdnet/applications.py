07deafaa619575408e996fcd58844a9d79021b11
quantmind <luca.sbardella@citi.com>
2010-11-08 17:34:13 +0000

    better monitor

--- stdnet/contrib/djstdnet/applications.py
+++ stdnet/contrib/djstdnet/applications.py
@@ -1,18 +1,25 @@
+from djpcms.views import appsite, appview
 
-from djpcms.views import appsite
-
-from stdnet.contrib.djstdnet.views import RedisHomeView, RedisDbView, RedisDbFlushView
+from stdnet.contrib.djstdnet import views
 
 
 class StdnetMonitorApplication(appsite.ApplicationBase):
     name = 'Stdnet Monitor'
     list_per_page = 100
     
-    home  = RedisHomeView(isplugin = True, isapp = True)
-    db    = RedisDbView(regex = '(?P<db>\d+)', isapp = True)
-    flush = RedisDbFlushView(regex = 'flush', parent = 'db')
+    home  = views.RedisHomeView(isplugin = True, isapp = True)
+    db    = views.RedisDbView(regex = '(?P<db>\d+)', isapp = True)
+    flush = views.RedisDbFlushView(regex = 'flush', parent = 'db')
     
     def dburl(self, db):
         dbview = self.getview('db')
         djp = view(request, db = db)
         return djp.url
+    
+    
+class StdModelApplication(appsite.ModelApplication):
+    search      = appview.SearchView()
+    information = views.StdModelInformationView(regex = 'info')
+    add         = appview.AddView()
+    view        = appview.ViewView()
+    edit        = appview.EditView(isplugin = True)

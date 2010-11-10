6cdd696faa0e3a045c405d6285c0aa43becc11eb
lsbardel <luca.sbardella@gmail.com>
2010-11-10 14:52:43 +0000

    addeed shinx to github extension and preparing to host documentation on github

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,13 @@ version = stdnet.__version__
 release = version
 # -- General configuration -----------------------------------------------------
 
+sphinx_to_github = True
+sphinx_to_github_verbose = True
+
+
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.pngmath']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinxtogithub']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

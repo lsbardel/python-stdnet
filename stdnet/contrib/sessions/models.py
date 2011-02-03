84f938b556afa70d1491afe3915f6676d76fa1c4
quantmind <luca.sbardella@citi.com>
2011-02-03 13:46:41 +0000

    added group and permission in session

--- stdnet/contrib/sessions/models.py
+++ stdnet/contrib/sessions/models.py
@@ -2,7 +2,11 @@ import base64
 import os
 import time
 import random
-import cPickle as pickle
+import hashlib
+try:
+    import cPickle as pickle
+except:
+    import pickle
 
 from stdnet import orm
 from stdnet import ObjectNotFound
@@ -14,6 +18,21 @@ if hasattr(random, 'SystemRandom'):
 else:
     randrange = random.randrange
 MAX_SESSION_KEY = 18446744073709551616L     # 2 << 63
+
+UNUSABLE_PASSWORD = '!' # This will never be a valid hash
+
+
+def get_hexdigest(salt, raw_password):
+    return hashlib.sha1(salt + raw_password).hexdigest()
+
+
+def check_password(raw_password, enc_password):
+    """
+    Returns a boolean of whether the raw_password was correct. Handles
+    encryption formats behind the scenes.
+    """
+    salt, hsh = enc_password.split('$')
+    return hsh == get_hexdigest(salt, raw_password)
 
 
 class SuspiciousOperation(Exception):
@@ -65,16 +84,59 @@ class SessionManager(orm.Manager):
         return True
     
     
+class UserManager(orm.Manager):
+    
+    def create_user(self, username, password=None, email=None, superuser = False):
+        if email:
+            try:
+                email_name, domain_part = email.strip().split('@', 1)
+            except ValueError:
+                pass
+            else:
+                email = '@'.join([email_name, domain_part.lower()])
+        else:
+            email = ''
+
+        user = self.model(username=username,
+                          #email=email,
+                          superuser=superuser)
+
+        user.set_password(password)
+        return user.save()
+
+    def create_superuser(self, username, password = None, email = None):
+        return self.create_user(username, email, password, superuser = True)
+    
+    
 class User(orm.StdModel):
     username  = orm.SymbolField(unique = True)
     password  = orm.CharField(required = True)
+    is_active = orm.BooleanField(default = True)
     superuser = orm.BooleanField(default = False)
+    
+    objects = UserManager()
     
     def is_authenticated(self):
         return True
     
     def is_superuser(self):
         return self.superuser
+    
+    def set_password(self, raw_password):
+        if raw_password:
+            salt = get_hexdigest(str(random.random()), str(random.random()))[:5]
+            hsh = get_hexdigest(salt, raw_password)
+            self.password = '%s$%s' % (salt, hsh)
+        else:
+            self.set_unusable_password()
+            
+    def set_unusable_password(self):
+        # Sets a value that will never be a valid hash
+        self.password = UNUSABLE_PASSWORD
+            
+    def check_password(self, raw_password):
+        """Returns a boolean of whether the raw_password was correct."""
+        return check_password(raw_password, self.password)
     
     @classmethod
     def authenticate(cls, **credentials):
@@ -97,6 +159,19 @@ class AnonymousUser(object):
     def is_superuser(self):
         return False
 
+
+class Group(orm.StdModel):
+    name  = orm.SymbolField(unique = True)
+    users = orm.ManyToManyField(User, related_name = 'groups')
+    
+
+class Permission(orm.StdModel):
+    '''A general permission model'''
+    numeric_code = orm.IntegerField()
+    group = orm.ForeignKey(Group, required = False)
+    user = orm.ForeignKey(User, required = False)
+    model_or_object = orm.SymbolField()
+    
 
 class Session(orm.StdModel):
     '''A simple session model'''

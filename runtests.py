35286c49e0472b163bfcc391d81ece6fc41f5957
lsbardel <luca.sbardella@gmail.com>
2011-12-16 07:49:48 +0000

    fixed runtests. it was broken for some reasons

--- runtests.py
+++ runtests.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from stdnet.conf import settings
+from stdnet.utils import PPath
 
 from pulsar.apps.test import TestSuite, TestOptionPlugin
 from pulsar.apps.test.plugins import bench
@@ -19,7 +20,6 @@ class TestServer(TestOptionPlugin):
 if __name__ == '__main__':
     p = PPath(__file__)
     p.add(module = 'pulsar', up = 1, down = ('pulsar',))
-    
     suite = TestSuite(description = 'Stdnet Asynchronous test suite',
                       modules = ('tests','stdnet.apps'),
                       plugins = (TestServer(),bench.BenchMark(),)
